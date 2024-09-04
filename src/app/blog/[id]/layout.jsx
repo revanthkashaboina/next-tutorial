7508bd978fa449faacea7095dbc8e3d6f919066e
revanthkashaboina <revanthkashaboina99@gmail.com>
2024-09-04 23:04:14 +0530

    Initial Commit

---
+++ src/app/blog/[id]/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogLayout = ({children}) => {
+  return (
+    <div>
+        <h1>This is Blog layout Page</h1>
+        {children}</div>
+  )
+}
+
+export default BlogLayout
