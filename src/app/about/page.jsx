adbc482888a191a5b4a7d20b977623096252a7bf
revanthkashaboina <revanthkashaboina99@gmail.com>
2024-10-06 10:57:08 +0530

    added about page UI

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
+import Button from '@/components/Button/button'
 
 const About = () => {
   return (
     <div className={styles.container}>
-      <div>
-        <div></div>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src='https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' fill={true} alt='about page image' />
+        <div className={styles.imgText}>
+          <h1>Digital Storytellers</h1>
+          <h2>Handcrafting award winning digital experiences</h2>
+        </div>
       </div>
-      <div>
-        <div></div>
-        <div></div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who are we?</h1>
+          <p className={styles.desc}>Universia is a network that consists of 862 universities in 22 countries: Andorra, Argentina, Bolivia, Brazil, Chile, Colombia, Costa Rica, Dominican Republic, Ecuador, El Salvador, Guatemala, Honduras, Mexico, Nicaragua, Panama, Paraguay, Peru, Portugal, Spain, United States, Uruguay, and Venezuela.
+            <br />
+            <br />
+            The network operates a global website that provides services like job searching and themed news. Universia network represented 250,000 registered users as of 2021. Universia is a company of the Santander Group Jaume Pagès Fita has been the CEO of Universia since 2005.</p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Our History</h1>
+          <p className={styles.desc}>Universia was created in July 2000 as an internet initiative in the higher education sector.
+            <br/>
+            It is promoted by a group of Spanish universities with the support of the Spanish University Rectors' Board, the Spanish National Research Council and sponsored by the Santander Group.
+          </p>
+          <Button url="/contact" text = "Contact"/>
+        </div>
       </div>
     </div>
   )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from './page.module.css';
+import Button from "@/components/Button/button";
 // import Hero from 'public/hero.png'
 
 export default function Home() {
@@ -8,7 +9,8 @@ export default function Home() {
       <div className={styles.item}>
         <h1 className={styles.title}>Better Designs for your digital products.</h1>
         <p className={styles.desc}>Turning your Idea into Reality. We bring together the teams from the global tech industry.</p>
-        <button className={styles.button}>See Our Works</button>
+        {/* <button className={styles.button}>See Our Works</button> */}
+        <Button url='/portfolio' text = "See Our Works"/>
       </div>
       <div className={styles.item}>
         <Image src='/hero.png' height={500} width={500} alt="home page" />
