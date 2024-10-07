5ed5df35d0834944db4514af2d613001b70336c5
revanthkashaboina <revanthkashaboina99@gmail.com>
2024-10-07 22:42:51 +0530

    added portfolios category section

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,10 +1,69 @@
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 
 const Category = ({params}) => {
   // console.log("params=============", params);
   return (
-    <div>
-      <h1>{params.category}</h1>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Description</p>
+          <Button url="#" text="See More"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          className={styles.img} 
+          fill={true} 
+          src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" 
+          alt="Category Image"/>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Description</p>
+          <Button url="#" text="See More"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          className={styles.img} 
+          fill={true} 
+          src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" 
+          alt="Category Image"/>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Description</p>
+          <Button url="#" text="See More"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          className={styles.img} 
+          fill={true} 
+          src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" 
+          alt="Category Image"/>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Description</p>
+          <Button url="#" text="See More"/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+          className={styles.img} 
+          fill={true} 
+          src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" 
+          alt="Category Image"/>
+        </div>
+      </div>
     </div>
   )
 }
