2897599c933f4ae7d33fa00be7a777481c8b5a70
revanthkashaboina <revanthkashaboina99@gmail.com>
2024-10-08 14:57:15 +0530

    added UI for id section in blog

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import styles from './page.module.css'
+import Image from 'next/image';
 
 const BlogPost = () => {
   return (
-    <div>BlogPost</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>heading</h1>
+          <p className={styles.desc}>paragraph</p>
+          <div className={styles.author}>
+            <Image src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" width={40} height={40} className={styles.avatar}/>
+            <span className={styles.username}>username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+            <Image src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" fill={true} className={styles.image}/>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>Below description paragraph</p>
+      </div>
+    </div>
   )
 }
 
