fb55a36a407ab114d203bf5548781ed8c84569d4
revanthkashaboina <revanthkashaboina99@gmail.com>
2024-10-19 13:15:02 +0530

    added dynamic data for categories and added dynamic logic for dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,41 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
+import { notFound } from 'next/navigation'
+import useSWR from 'swr'
+
+//using of useEffect is underated currently. use react query or swr hook from next.js which has built in packages like error handling and 
+//loading functionality.
 const Dashboard = () => {
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false)
+
+  // useEffect(()=>{
+  //   const getData = async() =>{
+  //     setIsLoading(true)
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {cache:"no-store"});
+
+  //     if(!res.ok){
+  //       setErr(true);
+  //     }
+
+  //     const data = await res.json()
+  //     setData(data)
+  //     setIsLoading(false)
+  //   }
+
+  //   getData();
+  // }, [])
+
+  // console.log("data==============", data);
+
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
+
+  console.log("data=========", data);
+
   return (
     <div className={styles.container}>Dashboard</div>
   )
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,68 +2,42 @@ import React from 'react'
 import styles from './page.module.css'
 import Button from '@/components/Button/Button'
 import Image from 'next/image'
+import { items } from './data'
+import { notFound } from 'next/navigation'
+
+const getData = (cat) =>{
+  const data = items[cat];
+
+  if(data){
+    return data
+  }
+
+  return notFound();
+}
 
 const Category = ({params}) => {
-  // console.log("params=============", params);
+
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
+
+      {data.map((item)=>(
+        <div className={styles.item} key={item.id}>
         <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
+          <h1 className={styles.title}>{item.title}</h1>
+          <p className={styles.desc}>{item.desc}</p>
           <Button url="#" text="See More"/>
         </div>
         <div className={styles.imgContainer}>
           <Image 
           className={styles.img} 
           fill={true} 
-          src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" 
+          src={item.image} 
           alt="Category Image"/>
         </div>
       </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button url="#" text="See More"/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-          className={styles.img} 
-          fill={true} 
-          src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" 
-          alt="Category Image"/>
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button url="#" text="See More"/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-          className={styles.img} 
-          fill={true} 
-          src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" 
-          alt="Category Image"/>
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-          <Button url="#" text="See More"/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-          className={styles.img} 
-          fill={true} 
-          src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" 
-          alt="Category Image"/>
-        </div>
-      </div>
+        ))}
     </div>
   )
 }
